57becb71005947c3e9352881ace9ffc2f5045247
Chris Boone <cboone1337@gmail.com>
2016-04-26 19:42:45 -0500

    server adds message created at

--- common/models/message.js
+++ common/models/message.js
@@ -1,3 +1,18 @@
 module.exports = function(Message) {
+  Message.beforeRemote('create', function(context, user, next) {
+    var req = context.req;
 
+    req.body.data.attributes['created-at'] = Date.now();
+    req.body.data.relationships.chatter.data = {
+      id: req.accessToken.userId,
+    };
+
+    if (!req.body.data.relationships.room.data) {
+      req.body.data.relationships.room.data = {
+        id: 1,
+      };
+    }
+
+    next();
+  });
 };
